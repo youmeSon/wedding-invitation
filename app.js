f2062a4e20948b8888f7ee7df9c702aa255dd649
Nicholas Oakes <32389274+nickoakes@users.noreply.github.com>
2023-03-02 23:42:38 +0000

    Map select controls removed.

--- app.js
+++ app.js
@@ -26,22 +26,3 @@ changeLanguage = () => {
         });
     }
 }
-
-mapSelect = mapType => {
-    const googleButton = document.getElementById('googleMapSelect'),
-          naverButton = document.getElementById('naverMapSelect'),
-          googleMap = document.getElementById('googleMap'),
-          naverMap = document.getElementById('naverMap');
-
-    if (mapType == 'google') {
-        naverMap.classList.add('d-none');
-        googleMap.classList.remove('d-none');
-        googleButton.classList.add('selected');
-        naverButton.classList.remove('selected');
-    } else {
-        naverMap.classList.remove('d-none');
-        googleMap.classList.add('d-none');
-        googleButton.classList.remove('selected');
-        naverButton.classList.add('selected');
-    }
-}
