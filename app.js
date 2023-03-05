c137ad46d8538b020edf0fcde0b93ec150d67f89
Nicholas Oakes <32389274+nickoakes@users.noreply.github.com>
2023-03-05 12:38:50 +0000

    Scroll into view functionality implemented.

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const titleTextKor = "유미와 닉의 결혼식",
   titleTextEn = "Youme and Nick's Wedding",
-  sakura = new Sakura("body"),
+  // sakura = new Sakura("body"),
   msPerDay = 86400000,
   timeRemaining = Math.round((new Date("04-23-2023") - new Date()) / msPerDay),
-  timeRemainingEl = document.getElementById("timeRemaining");
+  timeRemainingEl = document.getElementById("timeRemaining"),
+  copyMessageTimeoutLength = 1500,
+  scrollIntoViewThreshold = 0.9; //Element scrolls into view when 10% of it is above the bottom of the viewport
 
-let currentLanguage = "kor";
-let bankAccount = document.querySelector("#bank-account");
+let currentLanguage = "kor",
+    bankAccount = document.querySelector("#bank-account");
 
 timeRemainingEl.dataset.en = `(${timeRemaining} days left!)`;
 timeRemainingEl.dataset.kor = `(${timeRemaining}일 남았습니다!)`;
 timeRemainingEl.innerText = `( ${timeRemaining}일 남았습니다! )`;
+
+//Slick slider
+
+$('#journeyCarousel').slick({
+  autoplay: true
+});
+
+//Scroll elements into view
+
+document.addEventListener('scroll', fadeElementsIntoView);
+
+function fadeElementsIntoView () {
+  const scrollIntoViewElements = document.querySelectorAll('.scroll-into-view');
+  if (scrollIntoViewElements.length == 0) document.removeEventListener('scroll', fadeElementsIntoView); //Unbind event once all elements are visible
+  scrollIntoViewElements.forEach(element => {
+    if (isInViewport(element)) {
+      setTimeout(() => {
+        element.classList.remove('scroll-into-view');
+      }, 1000);
+    }
+  });
+}
+
+isInViewport = element => {
+  return element.getBoundingClientRect().top < window.innerHeight * scrollIntoViewThreshold;
+};
+
+//UI controls
 
 changeLanguage = () => {
   if (currentLanguage == "en") {
@@ -27,7 +57,6 @@ changeLanguage = () => {
     document.title = titleTextEn;
     document.querySelectorAll(".text-element").forEach((element) => {
       element.innerText = element.dataset.en;
-      bankAccount.classList.add("hide");
     });
     document.querySelectorAll('.en-hide').forEach(element => {
       element.classList.add('hide');
@@ -35,11 +64,21 @@ changeLanguage = () => {
   }
 };
 
-function copy(e) {
+copy = e => {
   const originalText = e.innerText;
   navigator.clipboard.writeText(e.dataset.accountno);
   e.innerText = '복사 됬습니다!';
   setTimeout(() => {
     e.innerText = originalText;
-  }, 3000);
+  }, copyMessageTimeoutLength);
+};
+
+//Deep linking
+
+const urlParams = new URLSearchParams(window.location.search),
+      language = urlParams.get('lang');
+
+if (language !== null) {
+  currentLanguage = language == 'en' ? 'kor' : 'en';
+  changeLanguage();
 }
