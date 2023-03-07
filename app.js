f7feddc03e070a671d11e632f736a45abf1073b7
Youme <rosydarling@naver.com>
2023-03-07 21:48:54 +0000

    carousel option added

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const titleTextKor = "유미와 닉의 결혼식",
   titleTextEn = "Youme and Nick's Wedding",
-  sakura = new Sakura("body"),
   msPerDay = 86400000,
   timeRemaining = Math.round((new Date("04-23-2023") - new Date()) / msPerDay),
   timeRemainingEl = document.getElementById("timeRemaining"),
   copyMessageTimeoutLength = 1500,
   scrollIntoViewThreshold = 0.9; //Element scrolls into view when 10% of it is above the bottom of the viewport
 
+// Sakura 
+let sakura = new Sakura('body', {
+  colors: [
+      {
+          gradientColorStart: 'rgba(255, 183, 197, 0.9)',
+          gradientColorEnd: 'rgba(255, 197, 208, 0.9)',
+          gradientColorDegree: 120,
+      },
+      {
+          gradientColorStart: 'rgba(255,189,189)',
+          gradientColorEnd: 'rgba(227,170,181)',
+          gradientColorDegree: 120,
+      },
+      {
+          gradientColorStart: 'rgba(212,152,163)',
+          gradientColorEnd: 'rgba(242,185,196)',
+          gradientColorDegree: 120,
+      },
+  ],
+  delay: 200,
+  fallSpeed: 2,
+
+});
 let currentLanguage = "kor",
     bankAccount = document.querySelector("#bank-account");
 
@@ -17,7 +39,13 @@ timeRemainingEl.innerText = `( ${timeRemaining}일 남았습니다! )`;
 //Slick slider
 
 $('#journeyCarousel').slick({
-  autoplay: true
+  autoplay: true,
+  autoplaySpeed: 3000,
+  fade: true,
+  infinite: true,
+  lazyLoad: "ondemand",
+  slidesToShow: 1,
+  cssEase: 'linear'
 });
 
 //Scroll elements into view
